cecc42eb0cdea5988484c1d78ac9d32adb10b299
BeLikeYEJOR <yejor599@gmail.com>
2024-12-11 16:41:16 -0800

    1

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { joke } = require("./jokes.js");
 
 let app = express();
 
 const foods = { 1: "banana", 2: "Apple", 3: "Orange", 4: "Mandarin" };
+
+app.get("/random-joke", (req, res) => {
+  let newjoke = joke[Math.floor(Math.random() * joke.length)];
+  res.json({ joke: newjoke });
+});
+
+app.get("/jokes", (req, res) => {
+  res.json({ jokes: joke });
+});
 
 //GET localhost:8080/greeting
 app.get("/greeting", (req, res) => {
@@ -15,9 +25,15 @@ app.get("/foods", (req, res) => {
 
 app.get("/foods/:id", (req, res) => {
   const id = req.params.id;
-  res.json({
-    food: foods[parseInt(id)],
-  });
+  if (id > 0 || id >= foods.length) {
+    res.json({
+      food: foods[parseInt(id)],
+    });
+  } else {
+    res.json({
+      error: "745 error, food id not found",
+    });
+  }
 });
 
 app.listen(8080, () => console.log("listning"));
